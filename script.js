88ac594d6536ac4d156d581fcf7a70f1cada2ab6
Vincenzo <158520146+Vinx92@users.noreply.github.com>
2024-03-13 10:49:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-function aggiungi() {
+let arrayTODO = [];
+const FORM = document.getElementById("form")
+FORM.addEventListener("submit", (event) => aggiungi(event))
+
+function aggiungi(event) {
+  event.preventDefault();
+  const OGGETTO = document.getElementById("oggetto");
+  if (OGGETTO.value === '') {
+    return
+  }
+
+  if (arrayTODO.includes(OGGETTO.value)) {
+    return
+  }
+
+  arrayTODO.push(OGGETTO.value)
+
   const DIV_ELEMENTO = document.createElement("div");
   DIV_ELEMENTO.className = "elemento";
   document.body.appendChild(DIV_ELEMENTO);
 
-  const OGGETTO = document.getElementById("oggetto").value;
   const SPAN = document.createElement("span");
-  SPAN.textContent = OGGETTO;
+  SPAN.textContent = OGGETTO.value;
   DIV_ELEMENTO.appendChild(SPAN);
 
   const DIV_ICONE = document.createElement("div");
@@ -14,8 +29,7 @@ function aggiungi() {
 
   const IMG_CHECK = document.createElement("img");
   IMG_CHECK.addEventListener("click", () => {
-    SPAN.style.textDecoration = "line-through";
-    DIV_ELEMENTO.style.backgroundColor = "red";
+    SPAN.classList.toggle('check')
   });
 
   IMG_CHECK.src = "assets/check-16.png";
@@ -25,8 +39,14 @@ function aggiungi() {
   const IMG_CROSS = document.createElement("img");
   IMG_CROSS.addEventListener("click", () => {
     DIV_ELEMENTO.remove();
+    for (let i = 0; i <= arrayTODO.length; i++) {
+      if (arrayTODO.indexOf(OGGETTO.value) === i) {
+        arrayTODO.splice(i, 1)
+      }
+    }
   });
   IMG_CROSS.src = "assets/cross-16.png";
   IMG_CROSS.alt = "icona croce";
   DIV_ICONE.appendChild(IMG_CROSS);
+  OGGETTO.value = ""
 }
