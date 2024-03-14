e4ab5c9bd126d7516ef9140907d6985f133daecb
Vincenzo <158520146+Vinx92@users.noreply.github.com>
2024-03-14 14:17:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,58 @@
-let arrayTODO = [];
-const FORM = document.getElementById("form")
-FORM.addEventListener("submit", (event) => aggiungi(event))
+const MODULO = document.getElementById("modulo");
+const OGGETTO = document.getElementById("oggetto");
+const INSERISCI = document.getElementById("inserisci");
+let arrayCoseDaFare = [];
+console.log(arrayCoseDaFare);
 
-function aggiungi(event) {
-  event.preventDefault();
-  const OGGETTO = document.getElementById("oggetto");
-  if (OGGETTO.value === '') {
-    return
+MODULO.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+INSERISCI.addEventListener("click", () => {
+  if (OGGETTO.value === "") {
+    return;
   }
 
-  if (arrayTODO.includes(OGGETTO.value)) {
-    return
+  if(arrayCoseDaFare.includes(OGGETTO.value) === true){
+    return;
   }
 
-  arrayTODO.push(OGGETTO.value)
+  arrayCoseDaFare.push(OGGETTO.value)
+  console.log(arrayCoseDaFare)
 
-  const DIV_ELEMENTO = document.createElement("div");
-  DIV_ELEMENTO.className = "elemento";
-  document.body.appendChild(DIV_ELEMENTO);
+  const ELEMENTO = document.createElement("div");
+  ELEMENTO.className = "elemento";
+  document.body.appendChild(ELEMENTO);
 
   const SPAN = document.createElement("span");
   SPAN.textContent = OGGETTO.value;
-  DIV_ELEMENTO.appendChild(SPAN);
+  ELEMENTO.appendChild(SPAN);
 
-  const DIV_ICONE = document.createElement("div");
-  DIV_ICONE.className = "icone";
-  DIV_ELEMENTO.appendChild(DIV_ICONE);
+  const ICONE = document.createElement("div");
+  ICONE.className = "icone";
+  ELEMENTO.appendChild(ICONE);
 
   const IMG_CHECK = document.createElement("img");
-  IMG_CHECK.addEventListener("click", () => {
-    SPAN.classList.toggle('check')
-  });
-
   IMG_CHECK.src = "assets/check-16.png";
   IMG_CHECK.alt = "icona check";
-  DIV_ICONE.appendChild(IMG_CHECK);
+  ICONE.appendChild(IMG_CHECK);
+  IMG_CHECK.addEventListener("click", () => {
+    SPAN.classList.toggle('fatto')
+  });
 
   const IMG_CROSS = document.createElement("img");
-  IMG_CROSS.addEventListener("click", () => {
-    DIV_ELEMENTO.remove();
-    for (let i = 0; i <= arrayTODO.length; i++) {
-      if (arrayTODO.indexOf(OGGETTO.value) === i) {
-        arrayTODO.splice(i, 1)
-      }
-    }
-  });
   IMG_CROSS.src = "assets/cross-16.png";
-  IMG_CROSS.alt = "icona croce";
-  DIV_ICONE.appendChild(IMG_CROSS);
-  OGGETTO.value = ""
-}
+  IMG_CROSS.alt = "icona cross";
+  ICONE.appendChild(IMG_CROSS);
+  IMG_CROSS.addEventListener('click', () => {
+    for (let i = 0; i < arrayCoseDaFare.length; i++) {
+        if(arrayCoseDaFare[i] === SPAN.textContent){
+            arrayCoseDaFare.splice(i, 1)
+            console.log(arrayCoseDaFare)
+        }
+    }
+    ELEMENTO.remove()
+  })
+
+  OGGETTO.value = "";
+});
